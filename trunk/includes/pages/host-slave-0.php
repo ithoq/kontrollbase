c19a72e51690e69b1ebb21f356967fe7a779aba3
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-04-13 20:57:23 +0000

    updated reporter to utilize correct socket variable

--- trunk/includes/pages/host-slave-0.php
+++ trunk/includes/pages/host-slave-0.php
@@ -3,6 +3,27 @@
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Kontrollbase 2.0.1 - MySQL Monitoring</title>
 
-<link rel="stylesheet" type="text/css" media="all" href="http://kontrollbase.npr.org/userguide/css/userguide-nofluff.css" />
+<link rel="stylesheet" type="text/css" href="http://192.168.0.20/kontrollbase/includes/style.css" />
+<link rel="stylesheet" type="text/css" media="all" href="http://192.168.0.20/kontrollbase/userguide/css/userguide-nofluff.css" />
 </head>
-<body><div id='content'><table><tr><td><h1>This server is not setup as a slave.</h1></td></tr></div></table></body></html>
+<body><table><div id='content'><tr><td><h1>Replication Information</h1></td></tr><tr><td>Slave SQL_Thread: YES</td></tr><tr><td>Slave I/O_Thread: YES</td></tr><tr><td>	<!-- START Code Block for Chart kontrollbase -->
+	<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase=http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"  width="240" height="130" id="kontrollbase">
+		<param name="allowScriptAccess" value="always" />
+		<param name="movie" value="http://192.168.0.20/kontrollbase/includes/FCF_Line.swf"/>		
+		<param name="FlashVars" value="&chartWidth=240&chartHeight=130&dataXML=<graph caption='Seconds_Behind_Master' xAxisName='' yAxisName='seconds' 
+showValues='0' rotateNames='1' decimalPrecision='2' showLimits='1' animation='1'
+showgridbg='1' showhovercap='1' showColumnShadow='1' 
+shadowYShift='0' shadowXShift='1' showAnchors='1' anchorRadius='2' anchorBgColor='b8b8b8' anchorScale='0' anchorAlpha='40' 
+showAlternateHGridColor='1' AlternateHGridColor='ff5904' divLineColor='ff5904' divLineAlpha='20' alternateHGridAlpha='5' 
+canvasBorderColor='b8b8b8' baseFontColor='666666' lineColor='99ccff' lineThickness='1'>
+</graph>" />
+		<param name="quality" value="high" />
+		<embed src="http://192.168.0.20/kontrollbase/includes/FCF_Line.swf" FlashVars="&chartWidth=240&chartHeight=130&dataXML=<graph caption='Seconds_Behind_Master' xAxisName='' yAxisName='seconds' 
+showValues='0' rotateNames='1' decimalPrecision='2' showLimits='1' animation='1'
+showgridbg='1' showhovercap='1' showColumnShadow='1' 
+shadowYShift='0' shadowXShift='1' showAnchors='1' anchorRadius='2' anchorBgColor='b8b8b8' anchorScale='0' anchorAlpha='40' 
+showAlternateHGridColor='1' AlternateHGridColor='ff5904' divLineColor='ff5904' divLineAlpha='20' alternateHGridAlpha='5' 
+canvasBorderColor='b8b8b8' baseFontColor='666666' lineColor='99ccff' lineThickness='1'>
+</graph>" quality="high" width="240" height="130" name="kontrollbase" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+	</object>
+	<!-- END Code Block for Chart kontrollbase --></td></tr></table></body></html>
