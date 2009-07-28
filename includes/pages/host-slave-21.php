6a5aa4060c3d51f488c81a87930a57ce000115d9
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-07-28 17:28:18 +0000

    upgraded to extjs-3.0.0

--- includes/pages/host-slave-21.php
+++ includes/pages/host-slave-21.php
@@ -1,8 +1,9 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Kontrollbase 2.0.1 - MySQL Monitoring</title>
 
-<link rel="stylesheet" type="text/css" media="all" href="http://192.168.0.20/kontrollbase/userguide/css/userguide-nofluff.css" />
+<link rel="stylesheet" type="text/css" media="all" href="http://10.128.10.22/kontrollbase-googlecode/userguide/css/userguide-nofluff.css" />
 </head>
 <body><div id='content'><table><tr><td><h1>This server is not setup as a slave.</h1></td></tr></div></table></body></html>
