c421eeb726036bb79906f09fc8756d08c19f0936
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-08-02 18:39:45 +0000

    added the btn.gif file for the slate theme button fix. this is the second time I've added it and svn had not picked it up - along with multiple other images that I've had to commit several times. I don't know what the deal is with svn lately but it's getting old. Anyway, here's the URL for the fix instructions in case I need to reference it again:  http://extjs.com/forum/showthread.php?p=336162

--- includes/pages/session.php
+++ includes/pages/session.php
@@ -4,15 +4,15 @@
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Kontrollbase 2.0.1 - MySQL Monitoring</title>
 
-<link rel="stylesheet" type="text/css" href="http://10.128.10.22/kontrollbase-googlecode/includes/style.css" />
-<link rel="stylesheet" type="text/css" media="all" href="http://10.128.10.22/kontrollbase-googlecode/userguide/css/userguide-nofluff.css" />
+<link rel="stylesheet" type="text/css" href="http://human.themattreid.com/kontrollbase/includes/style.css" />
+<link rel="stylesheet" type="text/css" media="all" href="http://human.themattreid.com/kontrollbase/userguide/css/userguide-nofluff.css" />
 </head>
 <body>
 <table>
-<tr><td>User: </td><td>mreid</td></tr>
+<tr><td>User: </td><td>admin</td></tr>
 <tr><td>Client: </td><td>System User</td></tr>
 <tr><td>Role: </td><td>Admin</td></tr>
-<tr><td>Logout: </td><td><a href="http://10.128.10.22/kontrollbase-googlecode/index.php/login/logout/" target="_self"><img src="http://10.128.10.22/kontrollbase-googlecode/includes/images/off.png" width="20px" height="20px"></a></td></tr>
+<tr><td>Logout: </td><td><a href="http://human.themattreid.com/kontrollbase/index.php/login/logout/" target="_self"><img src="http://human.themattreid.com/kontrollbase/includes/images/off.png" width="20px" height="20px"></a></td></tr>
 </table>
 </body>
 </html>
