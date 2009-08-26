fac52826a53504a17ac5a49db6ea332d9384e44b
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-08-26 19:56:26 +0000

    removed cruft

--- trunk/includes/pages/host-slave-13.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Kontrollbase 2.0.1 - MySQL Monitoring</title>
-
-<link rel="stylesheet" type="text/css" media="all" href="http://demo01.kontrollbase.com/userguide/css/userguide-nofluff.css" />
-</head>
-<body><div id='content'><table><tr><td><h1>This server is not setup as a slave.</h1></td></tr></div></table></body></html>
