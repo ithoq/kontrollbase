89d5566d1a5ff239d2097c8ce7aba52f6a1a1d8a
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-09-03 23:49:03 +0000

    updated support methods on userguide

--- trunk/userguide/support-contact.php
+++ trunk/userguide/support-contact.php
@@ -16,9 +16,17 @@ There are several methods. We're not a 24x7 operation since this is a two man pr
 
 <h2>Support Methods</h2>
 <ul>
-<li>Email: kontact@kontrollsoft.com</li>
-<li>AIM: username "kontrollsoft" on AOL Instant Messenger</li> 
-<li>IRC: you can find Matt in the #mysql channel on server irc.freenode.net as /nick "Lumberg"</li>
+<li>Support Forum: http://kontrollsoft.com/forum</li>
+<li>Kontrollsoft Bug Tracking: http://code.google.com/p/kontrollbase/issues/list</li> 
+<li>AIM: "kontrollsoft" - available 9am-5pm PST</li> 
+<li>YIM: "kontrollsoft" - available 9am-5pm PST</li> 
+<li>ICQ: "586343417" - available 9am-5pm PST</li> 
+<li>GTalk: support@kontrollsoft. available 9am-5pm PST</li> 
+<li>Jabber: support@kontrollsoft. available 9am-5pm PST</li> 
+<li>IRC: irc.freenode.net, channel #kontrollbase</li> 
+<li>Mail: support at kontrollsoft dot com</li> 
+<li>RSS: Keep up to date on news postings - http://feeds2.feedburner.com/Kontrollsoft</li> 
+<li>Pastebin: need to paste us some code - http://kontrollsoft.pastebin.com</li> 
 </ul>
 
 </div>
