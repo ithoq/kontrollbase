42433ac6232ee4bfd548d016b7a5e9552d6e9fbd
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-09-11 00:46:07 +0000

    added RSS news feed upon login for announcements

--- system/application/controllers/news.php
+++ system/application/controllers/news.php
@@ -19,40 +19,34 @@ class News extends Controller {
 	}
 
 	function index() {
-	  $basedir = $this->config->item('base_dir');
-	  $xmlurl = read_file("http://kontrollsoft.com/feed/rss");
-	  
-	  $rss =  simplexml_load_file("http://kontrollsoft.com/feed");
+	  $rss =  simplexml_load_file("http://kontrollsoft.com/archives/category/announcement/feed");
+	  $items= '';
 	  if ($rss) {
-	    $string = "<table>\n";
+	    $i=1;
+	    $c=count($rss->channel->item);
 	    foreach ($rss->channel->item as $item) {
 	      $date = $item->pubDate; 
+	      $date = substr($date, 0, -15);
 	      $title = $item->title;
 	      $desc = $item->description;
 	      $link = $item->link;
-	      
-	      $string .= "<tr><td><strong>$title</strong></td></tr>\n";
-	      $string .= "<tr><td>$date</td></tr>\n";
-	      $string .= "<tr><td>$desc</td></tr>\n";
-	      $string .= "<tr><td>$link</td></tr>\n";
-	      
-	      $catcount=0;
-	      $category='';
-	      foreach($item->category as $cat) {
-		if($category == '') {
-		  $category=$cat[$catcount];
-		}
-		else {
-		  $category = $category.",".$cat[$catcount];
-		}
-	      }
-	      $string .= "<tr><td>$category</td></tr>\n";
-	    }        
-	    $g['news'] = $string;
+	      $items .= "<div class=\"x-tab\" title=\"$date\">
+                          <p><b>$title</b></p>
+                          <p>&nbsp;</p>
+                          <p>$desc</p>
+                          <p>&nbsp;</p> 
+                          <p><a href='$link' target='_blank'>$link</a></p>
+                          </div>\n";
+	      $i++;
+	    }
+	    $g['root'] = $this->config->item('base_url');
+	    $g['news'] = $items;
+	    $g['total'] = $c;
 	    $this->load->view('news/current',$g);
 	  }
 	  else {
 	    $string = "<table><tr><td>No news today</td></tr></table>";
+	    $g['root'] = $this->config->item('base_url');
 	    $g['news'] = $string;
             $this->load->view('news/current',$g);
 	  }
