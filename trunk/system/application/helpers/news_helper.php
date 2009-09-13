0e2d009fbe87be7dd2c01439ee830d607d00a955
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-09-13 23:17:55 +0000

    added news helper

---
+++ trunk/system/application/helpers/news_helper.php
@@ -0,0 +1,51 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+  /**
+   * Kontrollbase
+   *
+   * An open source MySQL monitoring and analytics application
+   *
+   * @package Kontrollbase
+   * @author Matt Reid
+   * @copyright Copyright (c) 2009 Matt Reid, Kontrollbase LLC
+   * @license http://kontrollsoft.com/kontrollbase/userguide/general-LICENSE.php
+   * @link http://kontrollbase.com
+   */
+
+function news() {
+  log_message('debug', "news helper function starting");
+  $xmlfile = "http://kontrollsoft.com/archives/category/announcement/feed";
+  $rss =  simplexml_load_file($xmlfile);
+  $items= '';
+  if ($rss) {
+    foreach($rss->channel->item as $item) {
+      $date = $item->pubDate; 
+      $date = substr($date, 0, -15);
+      $date = trim($date);
+      $title = $item->title;
+      $title = trim($title);
+      $title = substr($title, 0, 160);
+      $desc = $item->description;
+      $desc = substr($desc, 0, 150);
+      $desc = trim($desc);
+      $desc .= "...";
+      $link = $item->link;
+      $link = trim($link);
+      
+      $items[] = array("date" => "$date",
+		       "title" => "$title",
+		       "desc" => "$desc",
+		       "link" => "<a href=\"$link\" target=\"_blank\">click for more</a>");
+    }
+    return $items;    
+  }
+  else {
+    $items = array(array("date" => "NULL",
+			 "title" => "NULL",
+			 "desc" => "NULL",
+			 "link" => "NULL"));
+    return $items;
+  }
+}
+
+?>
