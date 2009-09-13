92e7f7e8805154cdd2ab84a961924028f4695353
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-09-13 23:34:24 +0000

    added forum link to toolbar on host page

--- system/application/views/header_host.php
+++ system/application/views/header_host.php
@@ -207,6 +207,27 @@ print "
      text: 'Goto Environment',
      handler: function() {
        window.location = '$nroot/index.php';}
+   },
+   {
+     xtype: 'tbseparator'
+   },
+   {
+     xtype: 'tbbutton',
+     text: 'Forums',
+     handler: function() {
+       window.location = 'http://kontrollsoft.com/forum';}
+   },
+   {
+     xtype: 'tbseparator'
+   },
+   {
+     xtype: 'tbbutton',
+     id: this.id + 'AuditButton',
+     cls: 'x-btn-icon audit-button',
+     tooltip: 'Contact Support',
+     handler: function() {
+       window.location = 'http://kontrollsoft.com/support';},
+     scope: this,
    }]},
    {
      region:'west',
