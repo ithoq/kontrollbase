e0359e9c883f68fa5c5d007998f954a2c8562dd2
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2009-12-10 18:43:54 +0000

    addressing issue #87: browser detection for IE

--- system/application/views/header_login.php
+++ system/application/views/header_login.php
@@ -36,67 +36,77 @@ print<<<HEAD
 <script type="text/javascript">
   Ext.onReady(function(){
 		Ext.QuickTips.init();
- 
-		var login = new Ext.FormPanel({ 
-		  renderTo: "loginbox",
-		      buttonAlign: 'center',
-		      autoHeight: true,
-		      labelWidth:80,
-		      url:'$nroot/index.php/login/auth/', 
-		      frame:true, 
-		      title:'<img src="$nroot/includes/images/kontrollbase_logo-trans32.png" align="center">',		      
-		      defaultType:'textfield',
-		      monitorValid:true,
-		      items:[{ 
-		    fieldLabel:'Username', 
-			name:'system_user_name', 
-			allowBlank:false 
-			},{ 
-			     fieldLabel:'Password', 
-				 name:'system_user_pass', 
-				 inputType:'password', 
-				 allowBlank:false 
-				 }],
- 
-		      buttons:[{ 
-		    text:'Login',
-			formBind: true, 
-			handler:function(){ 
-			login.getForm().submit({ 
-			  method:'POST', 
-			      waitTitle:'Connecting', 
-			      waitMsg:'Authenticating...',
- 
-			      success:function(){
-			      var redirect = '$nroot/index.php/main/';
-			      window.location = redirect;
-			    },
- 
-			      failure:function(form, action){ 
-			      if(action.failureType == 'server'){ 
-                                obj = Ext.util.JSON.decode(action.response.responseText); 
-                                Ext.Msg.alert('Login Failed.', obj.errors.reason); 
-			      }else{ 
-                                Ext.Msg.alert('Warning!', 'Authentication server is unreachable : ' + action.response.responseText); 
+		if(Ext.isIE) {
+		  Ext.Msg.show({
+		    title:'Browser Alert',
+			msg: 'Internet Explorer is not supported by this application.<br>Please use a W3C standards compliant browser like Firefox, Safari, Camino, Chrome, etc.',
+			icon: Ext.MessageBox.INFO,
+			minWidth: 300,
+			minHeight: 100
+			});
+		}
+		else {
+		  var login = new Ext.FormPanel({ 
+		    renderTo: "loginbox",
+			buttonAlign: 'center',
+			autoHeight: true,
+			labelWidth:80,
+			url:'$nroot/index.php/login/auth/', 
+			frame:true, 
+			title:'<img src="$nroot/includes/images/kontrollbase_logo-trans32.png" align="center">',
+			defaultType:'textfield',
+			monitorValid:true,
+			items:[{ 
+		      fieldLabel:'Username', 
+			  name:'system_user_name', 
+			  allowBlank:false 
+			  },{ 
+			       fieldLabel:'Password', 
+				   name:'system_user_pass', 
+				   inputType:'password', 
+				   allowBlank:false 
+				   }],
+			
+			buttons:[{ 
+		      text:'Login',
+			  formBind: true, 
+			  handler:function(){ 
+			  login.getForm().submit({ 
+			    method:'POST', 
+				waitTitle:'Connecting', 
+				waitMsg:'Authenticating...',
+				
+				success:function(){
+				var redirect = '$nroot/index.php/main/';
+				window.location = redirect;
+			      },
+				
+				failure:function(form, action){ 
+				if(action.failureType == 'server'){ 
+				  obj = Ext.util.JSON.decode(action.response.responseText); 
+				  Ext.Msg.alert('Login Failed.', obj.errors.reason); 
+				}else{ 
+				  Ext.Msg.alert('Warning!', 'Authentication server is unreachable : ' + action.response.responseText); 
+				} 
+				login.getForm().reset(); 
 			      } 
-			      login.getForm().reset(); 
-			    } 
-			  }); 
-		      } 
-		    }] 
-		      });
-		var win = new Ext.Window({
-		  layout:'fit',
-                      width:300,
-                      height:150,
-                      closable: false,
-                      resizable: false,
-                      plain: true,
-                      border: false,
-		      items: [login]
-                      });
-                win.show();
-              });
+			    }); 
+			} 
+		      }] 
+			});
+		  var win = new Ext.Window({
+		    layout:'fit',
+			width:300,
+			height:150,
+			closable: false,
+			resizable: false,
+			plain: true,
+			border: false,
+			items: [login]
+			});
+		  win.show();
+		}
+	      });
 </script>
     
     </head>
