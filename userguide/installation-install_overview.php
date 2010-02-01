ef338b88f7b5d56100cdb23da6e801e09a45e150
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2010-02-01 20:53:30 +0000

    updated documentation to show simple method to install perl/php/etc requirements

--- userguide/installation-install_overview.php
+++ userguide/installation-install_overview.php
@@ -107,6 +107,45 @@ Prerequisite DBI.pm located
 Prerequisite /Math/Calc/Units.pm located
 All required Perl modules are available.</code>
 
+<h2>The Simplest Method for Installing Requirements</h2>
+The easiest way to do this is to use Redhat/Centos for your server. This is because RPMforge has all of the required modules as RPM files. You may find that Ubuntu/Debian has most or all of the packages in their various repositories but we don't keep very up to date on that platform since most enterprise servers that we encounter run Redhat and, as such, the application is built and developed on that OS. So, here are the instructions for the quickets method of installing all required dependencies.<br>
+<h3>Install the RPMforge repository RPM for your particular OS</h3>
+<strong>This is required since most of the RPMs are not found in the standard OS repositories.</strong><br>
+<code>http://dag.wieers.com/rpm/FAQ.php#B</code>
+
+<h3>Install MySQL client and server</h3>
+<strong>Not required if you are installing MySQL from alternate means</strong><br>
+<code>yum -y install mysql mysql-server</code>
+
+<h3>Install Apache and PHP requirements</h3>
+<strong>You might already have Apache installed but you may not have the other PHP modules installed</strong><br>
+<code>yum -y install httpd php php-mysql php-mbstring php-xml</code>
+
+<h3>Install Perl and Perl modules requirements</h3>
+<strong>You probably already have Perl installed but you definitely need the other modules.</strong><br>
+<code>yum -y install perl perl-DBD-mysql perl-DBI perl-Digest-HMAC perl-XML-LibXML perl-XML-Simple perl-XML-Parser perl-XML-SimpleObject perl-XML-LibXML-Common perl-Crypt-DES perl-Math-Calc-Units perl-Net-SNMP </code>
+
+<h3>For Reporter-CLI functionality if desired</h3>
+<code>yum -y install sqlite perl-DBD-SQLite</code>
+
+<h3>Check that all requirements are met</h3>
+<strong>Go to the bin/ directory and run the php script for checking requirements.</strong><br>
+<code>ROOT-(root@localhost)-(/usr/local/src/kontrollbase-read-only/bin)-ROOT
+ROOT-(0)> php ./kontroll-check_reqs.php 
+Starting...
+Beggining PHP prerequisite checks
+Prerequisite dom located
+Prerequisite mbstring located
+Prerequisite mysql located
+Prerequisite xml located
+All required PHP modules are available.
+Beginning Perl prerequisite checks
+Prerequisite XML/Parser.pm located
+Prerequisite XML/SimpleObject.pm located
+Prerequisite DBI.pm located
+Prerequisite /Math/Calc/Units.pm located
+All required Perl modules are available.</code>
+
 <h2>Webapp Installation</h2>
     The webapp is simply the visual interaction with the application. 
 Think of it as the display layer that makes all of the data able to be used and interpreted.<br>
