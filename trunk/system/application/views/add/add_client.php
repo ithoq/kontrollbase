358df47512588a9bbf647f47a2df056ae7df4568
themattreid <themattreid@868d43b0-22f0-11de-8691-b5df825c884c>
2010-02-04 20:40:32 +0000

    updated vtype to fix error in netmaskvalue

--- trunk/system/application/views/add/add_client.php
+++ trunk/system/application/views/add/add_client.php
@@ -38,7 +38,7 @@ print<<<HEAD
 Ext.form.VTypes["hostnameVal1"] = /^[a-zA-Z][-.a-zA-Z0-9]{0,254}$/;
 Ext.form.VTypes["hostnameVal2"] = /^[a-zA-Z]([-a-zA-Z0-9]{0,61}[a-zA-Z0-9]){0,1}([.][a-zA-Z]([-a-zA-Z0-9]{0,61}[a-zA-Z0-9]){0,1}){0,}$/;
 Ext.form.VTypes["ipVal"] = /^([1-9][0-9]{0,1}|1[013-9][0-9]|12[0-689]|2[01][0-9]|22[0-3])([.]([1-9]{0,1}[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){2}[.]([1-9][0-9]{0,1}|1[0-9]{2}|2[0-4][0-9]|25[0-4])$/;
-Ext.form.VTypes["netmaskVal"] = /^(128|192|224|24[08]|25[245].0.0.0)|(255.(0|128|192|224|24[08]|25[245]).0.0)|(255.255.(0|128|192|224|24[08]|25[245]).0)|(255.255.255.(0|128|192|224|24[08]|252))$/;
+Ext.form.VTypes["netmaskVal"] = /(^(128|192|224|24[08]|25[245])\.0\.0\.0$)|(^255\.(0|128|192|224|24[08]|25[245])\.0\.0$)|(^255\.255\.(0|128|192|224|24[08]|25[245])\.0$)|(^255\.255\.255\.(0|128|192|224|24[08]|252)$)/;
 Ext.form.VTypes["portVal"] = /^(0|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
 Ext.form.VTypes["multicastVal"] = /^((22[5-9]|23[0-9])([.](0|[1-9][0-9]{0,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])){3})|(224[.]([1-9][0-9]{0,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])([.](0|[1-9][0-9]{0,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])){2})|(224[.]0[.]([1-9][0-9]{0,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])([.](0|[1-9][0-9]{0,1}|1[0-9]{2}|2[0-4][0-9]|25[0-5])))$/;
 Ext.form.VTypes["usernameVal"] = /^[a-zA-Z][-_.a-zA-Z0-9]{0,30}$/;
@@ -90,6 +90,10 @@ Ext.form.VTypes["password"]=function(v){
 Ext.form.VTypes["passwordText"]="Invalid Password"
   Ext.form.VTypes["passwordMask"]=/./;
 
+Ext.form.VTypes["phone"] = /^(\d{3}[-]?){1,2}(\d{4})$/;
+Ext.form.VTypes["phoneMask"] = /[\d-]/;
+Ext.form.VTypes["phoneText"] = 'Not a valid phone number.  Must be in the format 123-4567 or 123-456-7890 (dashes optional)';
+
 Ext.form.VTypes.nameVal  = /^[a-zA-Z0-9_\-]+$/;
 Ext.form.VTypes.nameMask = /[A-Za-z0-9\- ]/;
 Ext.form.VTypes.nameText = 'Invalid client name.';
@@ -132,6 +136,7 @@ Ext.form.VTypes.name    = function(v){
 				 },
 			     {
 			     fieldLabel:'Phone',
+				 vtype: 'phone',
 				 name:'server_client_phone',
 				 inputType: 'text',
 				 width:250,
